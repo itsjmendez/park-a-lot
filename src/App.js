98ed48e58108122577114171bd5cc66630c5aeae
Josemanuel Mendez <jmocontactme@gmail.com>
2022-10-03 17:09:15 -0500

    Initial commit and project setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home/Index';
+import { Account } from './pages/Account/Index';
+import { Login } from './pages/Login/Index';
+import { Reservation } from './pages/Reservation/Index';
+import { History } from './pages/History/Index';
+import { Payments } from './pages/Payments/Index';
+import { Subscription } from './pages/Subscription/Index';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/history" element={<History />} />
+        <Route path="login" element={<Login />} />
+        <Route path="/payments" element={<Payments />} />
+        <Route path="reservation" element={<Reservation />} />
+        <Route path="subscription" element={<Subscription />} />
+      </Routes>
     </div>
   );
 }
